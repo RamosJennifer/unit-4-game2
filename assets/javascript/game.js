3d930f74584d86cc246bffadc20b1a9b9ce05d7a
Jennifer Ramos <jenniferramos4858@gmail.com>
2021-06-29 11:28:57 -0500

    creating a unique div and nesting a randomized number inside

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,13 @@
 var crystals = ["crystalOne","crystalTwo","crystalThree","crystalFour"];
 
 for (var i=0;i<crystals.length;i++){
+    // div with individual classes
+    var crystalValues = $("<div>");
+    crystalValues.attr("id",crystals[i]);
+    console.log(crystals[i]);
+    $("#crystalValues").append(crystalValues);
+    // adding the randomized number inside the div created above
     crystals[i] = (Math.floor((Math.random()*12)+1));
     console.log(crystals[i]);
-    $("#crystalValues").append("<div>" + crystals[i]);
+    $(crystalValues).append(crystals[i]);
 }
