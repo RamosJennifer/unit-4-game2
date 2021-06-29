e0d916dd31dcd7741af3b1691d3ef3f611b81983
Jennifer Ramos <jenniferramos4858@gmail.com>
2021-06-29 15:53:18 -0500

    printing randomized target score from 19-120 and user dynamic score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,31 @@
-var crystals = ["crystalOne","crystalTwo","crystalThree","crystalFour"];
+$(document).ready(function() {
+    var crystals = ["crystalOne","crystalTwo","crystalThree","crystalFour"];
+    var targetScore = (Math.floor((Math.random()*120)+19));
+    var userScore = 0;
 
-for (var i=0;i<crystals.length;i++){
-    // div with individual classes
-    var crystalValues = $("<div>");
-    crystalValues.attr("id",crystals[i]);
-    console.log(crystals[i]);
-    $("#crystalValues").append(crystalValues);
-    // adding the randomized number inside the div created above
-    crystals[i] = (Math.floor((Math.random()*12)+1));
-    console.log(crystals[i]);
-    $(crystalValues).append(crystals[i]);
-}
+    // Start Game
+    function initiateGame (){
+        $("#targetScore").text("Target Score: " + targetScore);
+        $("#userScore").text("Your Score: " + userScore);
+    }
+    initiateGame();
+
+    for (var i=0;i<crystals.length;i++){
+        // div with individual id's
+        var crystalValues = $("<button>");
+        crystalValues.attr("id",crystals[i]);
+        console.log(crystals[i]);
+        $("#crystalValues").append(crystalValues);
+        // adding the randomized number inside the div created above
+        crystals[i] = (Math.floor((Math.random()*12)+1));
+        console.log(crystals[i]);
+        $(crystalValues).attr("value",crystals[i]);
+        $(crystalValues).append(crystals[i]);
+    }
+    // When user clicks crystal with id crystalOne
+    $("#crystalOne").on("click", function() {
+        alert("You've clicked crystal one.");
+        userScore += parseInt($(this).val())
+        $("#userScore").text("Your Score: " + userScore);
+    });
+});
